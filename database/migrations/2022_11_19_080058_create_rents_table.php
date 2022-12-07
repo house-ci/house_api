61fa84673a5fa1429e5b1f40f70fbae879a859bb
Ben G. Seydou Ouattara <ben.ouattara@outlook.com>
2022-12-07 16:27:45 +0000

    update migration rent table to handle partial payment

--- database/migrations/2022_11_19_080058_create_rents_table.php
+++ database/migrations/2022_11_19_080058_create_rents_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -20,6 +19,7 @@ return new class extends Migration
             $table->string('year');
             $table->string('status')->default('PENDING');
             $table->integer('amount');
+            $table->integer('amount_paid')->default(0);
             $table->string('currency')->default('XOF');
             $table->dateTimeTz('paid_at')->nullable();
             $table->foreignUuid('leasing_id')->constrained();
